6366f058a5187e604c0ac786849d169aa50859e0
Luiz Camargo <luizhp.camargo@gmail.com>
2019-10-03 15:46:49 -0300

    chore: import view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,20 @@
 class EventsController < ApplicationController
-  before_action :load_instances
+  before_action :load_instances, only: :index
 
   def index; end
 
+  def new; end
+
+  def create
+    redirect_to new_event_path if event_params[:file].blank?
+    Importer::Cnab.new(event_params[:file]).import!
+  end
+
   private
+
+  def event_params
+    params.require(:event).permit(:file)
+  end
 
   def load_instances
     @events = Event.includes(:store).references(:store).all
